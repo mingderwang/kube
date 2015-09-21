c68b21deb90e1123559fca22ee24fc07633673b6
Ming-der Wang <ming@log4analytics.com>
2015-09-21 11:06:21 +0800

    add command line

--- main.go
+++ main.go
@@ -6,5 +6,46 @@ import (
 )
 
 func main() {
-	cli.NewApp().Run(os.Args)
+
+	app := cli.NewApp()
+	app.Name = "kube"
+	app.Usage = "cli for sharing kubernetes resource files repos and management"
+	app.Version = "0.0.1"
+
+	app.Commands = []cli.Command{
+		{
+			Name:    "search",
+			Aliases: []string{"s"},
+			Usage:   "Search global kubernetes resource files",
+			Action: func(c *cli.Context) {
+				println("searching")
+			},
+		},
+		{
+			Name:    "push",
+			Aliases: []string{"p"},
+			Usage:   "Push resource files to kube.hub with tag",
+			Action: func(c *cli.Context) {
+				println("pushing files")
+			},
+		},
+		{
+			Name:    "get",
+			Aliases: []string{"g"},
+			Usage:   "Get resource files from kube.hub by tag",
+			Action: func(c *cli.Context) {
+				println("downloading")
+			},
+		},
+		{
+			Name:    "like",
+			Aliases: []string{"star"},
+			Usage:   "Add a star for a specific resource tag",
+			Action: func(c *cli.Context) {
+				println("star +1")
+			},
+		},
+	}
+
+	app.Run(os.Args)
 }
