65e0baa475cd190b4d8012ca86b06cddfccff67a
Ming-der Wang <ming@log4analytics.com>
2015-09-21 13:29:26 +0800

    read one file for push

--- pushFile.go
+++ pushFile.go
@@ -1,5 +1,41 @@
 package main
 
+import (
+	"encoding/json"
+	"github.com/davecgh/go-spew/spew"
+	"io/ioutil"
+	"log"
+)
+
+type Gist struct {
+	Files       map[string]GistFile `json:"files"`
+	Public      bool                `json:"public"`
+	Description string              `json:"description"`
+}
+
+type GistFile struct {
+	Content string `json:"content"`
+}
+
 func pushFile(filename string, tag string, description string) {
 	println("pushing file....")
+	files := map[string]GistFile{}
+
+	content, err := ioutil.ReadFile(filename)
+	if err != nil {
+		log.Fatal("Read file error: ", err)
+	}
+	files[filename] = GistFile{string(content)}
+
+	gist := Gist{
+		files,
+		true,
+		description,
+	}
+	data, err := json.Marshal(gist)
+	if err != nil {
+		log.Fatal("JSON Error: ", err)
+	}
+	spew.Dump(gist)
+	spew.Dump(data)
 }
